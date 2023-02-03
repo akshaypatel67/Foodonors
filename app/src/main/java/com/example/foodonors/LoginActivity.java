04be6ec5347b73219befc2509dc7e8eec6e9e0e3
Bhargav <bhargavmodha612@gmail.com>
2023-02-04 04:18:22 +0530

    - Completed Login and Sign up

--- app/src/main/java/com/example/foodonors/LoginActivity.java
+++ app/src/main/java/com/example/foodonors/LoginActivity.java
@@ -1,14 +1,67 @@
 package com.example.foodonors;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.hbb20.CountryCodePicker;
+
 public class LoginActivity extends AppCompatActivity {
+    Button next;
+    TextInputLayout phoneNumber;
+    CountryCodePicker countryCodePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        next = findViewById(R.id.btn_next);
+        phoneNumber = findViewById(R.id.phone_num);
+        countryCodePicker = findViewById(R.id.country_code_picker);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CompleteSignup(view);
+            }
+        });
+    }
+
+
+    private void CompleteSignup(View view) {
+
+        if (!validatePhoneNumber()) {
+            return;
+        }
+
+        String _getUserEnteredPhoneNumber = phoneNumber.getEditText().getText().toString().trim();
+        String _phoneNo = "+" + countryCodePicker.getSelectedCountryCode() + _getUserEnteredPhoneNumber;
+
+        Intent i = new Intent(getApplicationContext(), VerifyOTP.class);
+        i.putExtra("phoneno", _phoneNo);
+
+        startActivity(i);
+    }
+
+    private boolean validatePhoneNumber() {
+
+        String val = phoneNumber.getEditText().getText().toString().trim();
+
+        if (val.isEmpty()) {
+            phoneNumber.setError("Field can not be empty");
+            return false;
+        } else if (!android.util.Patterns.PHONE.matcher(val).matches()) {
+            phoneNumber.setError("Enter valid phone number");
+            return false;
+        } else {
+            phoneNumber.setError(null);
+            phoneNumber.setErrorEnabled(false);
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/com/example/foodonors/VerifyOTP.java
@@ -0,0 +1,130 @@
+package com.example.foodonors;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.chaos.view.PinView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class VerifyOTP extends AppCompatActivity {
+    private static final String TAG = "";
+
+    PinView pinView;
+    String mVerificationId;
+    Button verifyOTP;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential credential) {
+            Log.d(TAG, "onVerificationCompleted:" + credential);
+            String code = credential.getSmsCode();
+            if (code != null) {
+                pinView.setText(code);
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Log.w(TAG, "onVerificationFailed", e);
+
+            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                Toast.makeText(VerifyOTP.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            } else if (e instanceof FirebaseTooManyRequestsException) {
+                Toast.makeText(VerifyOTP.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
+            Log.d(TAG, "onCodeSent:" + verificationId);
+            mVerificationId = verificationId;
+        }
+    };
+    String phoneNo;
+    TextView phoneNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_o_t_p);
+
+        pinView = findViewById(R.id.pin_view);
+        verifyOTP = findViewById(R.id.verify_otp);
+        phoneNum = findViewById(R.id.code_phone_num);
+
+        phoneNo = getIntent().getExtras().getString("phoneno");
+        sendVerificationCodeToUser(phoneNo);
+        phoneNum.setText(String.format("%s on %s", phoneNum.getText(), phoneNo));
+
+        verifyOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callNextScreenFromOTP(view);
+            }
+        });
+
+    }
+
+    private void callNextScreenFromOTP(View view) {
+        String code = pinView.getText().toString();
+        if (!code.isEmpty()) {
+            verifyCode(code);
+        }
+    }
+
+    private void sendVerificationCodeToUser(String phoneNo) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNo)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(VerifyOTP.this, "Verification not Completed! Try again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+}
